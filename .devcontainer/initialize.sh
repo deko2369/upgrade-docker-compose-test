658a6eb7d50b4bfb1467faecd6fb45e3795f72ab
deko2369 <deko2369@gmail.com>
2023-12-22 11:20:35 +0000

    fixed symbolic link

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -4,4 +4,4 @@ export SECRET=""
 
 curl -SL https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64 \
      -o /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
