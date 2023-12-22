aed0092e366d928ece3b83be6c052879a020f331
deko2369 <deko2369@gmail.com>
2023-12-22 11:14:26 +0000

    fix docker compose install command 2

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-apt update && apt install -y docker-compose-plugin
+curl -SL https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64 \
+     -o /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
